bf3b33ae5be230e0a51f1aa6d4983f4c1cec4da1
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-12 15:52:39 +0800

    Adds seed to Article

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Article.destroy_all
+
+Article.create(title: 'How To Win Finite Games With An Infinite Mindset', body: 'Finite and infinite games: Were defined by professor James P. Carse in his book Finite and Infinite Games: A Vision of Life as Play and Possibility, and expanded upon by Simon Sinek in his book, The Infinite Game.')
+Article.create(title: 'How to Turn Simon Sinek´s Golden Circle Into Coal, Or How to Wreck Yourself', body: 'This is the third article of the These Times Are Made for Misfits series. The first was How to Be a Misfit, and the second was Action Confidence. Please be aware of the following two articles, Developing Dual Awareness, and Illuminating the Blind Spot of Leadership.')
