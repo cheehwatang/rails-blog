73775cf967ad6a420b7451dab8c592a5893f7974
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-12 17:56:56 +0800

    Adjusts seed to attach photo to photos field

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Article.destroy_all
 
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
 article = Article.new(title: "NES", body: "A great console")
-article.photo.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
+article.photos.attach(io: file, filename: "nes.jpg", content_type: "image/jpg")
 article.save
 
 Article.create(title: 'How To Win Finite Games With An Infinite Mindset', body: 'Finite and infinite games: Were defined by professor James P. Carse in his book Finite and Infinite Games: A Vision of Life as Play and Possibility, and expanded upon by Simon Sinek in his book, The Infinite Game.')
