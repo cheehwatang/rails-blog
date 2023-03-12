f61a86adf7cb003afb73e50eeb6029571557343d
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-12 14:11:37 +0800

    Adds title and body to Article model

--- db/migrate/20230312060745_create_articles.rb
+++ db/migrate/20230312060745_create_articles.rb
@@ -1,6 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      t.string "title", null: false
+      t.text "body", null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_12_060745) do
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
